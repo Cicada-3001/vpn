4e71fb68c702d95cf1262e7728f8f543be4d0f00
Pablo Picasso Sancho <cicada3319@gmail.com>
2023-09-04 16:22:51 +0300

    next

--- components/Layout/Header.js
+++ components/Layout/Header.js
@@ -3,7 +3,8 @@ import Link from "next/link";
 // Import react scroll
 import { Link as LinkScroll } from "react-scroll";
 import ButtonOutline from "../misc/ButtonOutline.";
-import LogoVPN from "../../public/assets/Logo.svg";
+import Logo from "../../public/assets/Log.svg";
+import Image from "next/Image"
 
 const Header = () => {
   const [activeLink, setActiveLink] = useState(null);
@@ -23,7 +24,7 @@ const Header = () => {
       >
         <nav className="max-w-screen-xl px-6 sm:px-8 lg:px-16 mx-auto grid grid-flow-col py-3 sm:py-4">
           <div className="col-start-1 col-end-2 flex items-center">
-            <LogoVPN className="h-8 w-auto" />
+            <Image src="/assets/logo.png" className="h-8 w-auto" width={149} height={37} />
           </div>
           <ul className="hidden lg:flex col-start-4 col-end-8 text-black-500  items-center">
             <LinkScroll
